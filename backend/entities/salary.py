21b6e3d875a8cd03ac8c1d2ca1991b9b0d42bb6e
Phan Tien Tai <143255769+ptt3123@users.noreply.github.com>
2025-07-16 11:57:52 +0700

    Feat/change entities (#3)
    * Dev (#2)
    
    * feat: Update .gitignore, add .idea
    
    * Feat/build project structure (#1)
    
    * feat: Update .gitignore
    
    * feat: Build project structure
    
    * feat: Update DB and migrations

--- backend/entities/salary.py
+++ backend/entities/salary.py
@@ -6,8 +6,8 @@ class Salary(Base):
     __tablename__ = "salary"
 
     salary = Column(Integer, nullable=False)
-    allowance = Column(Integer, nullable=True)
-    reward = Column(Integer, nullable=True)
+    allowance = Column(Integer, nullable=False)
+    reward = Column(Integer, nullable=False)
     create_date = Column(DateTime, server_default=func.now())
     update_date = Column(DateTime, nullable=True, server_onupdate=func.now())
     detail = Column(String(255), nullable=True)
