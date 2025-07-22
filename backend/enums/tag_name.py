515a50449db6613456f485f4bfef08fc4ec4796e
Tai Phan <taip7547@gmail.com>
2025-07-22 08:46:11 +0700

    feat: delete trash

--- backend/enums/tag_name.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class TagName(str, Enum):
-    LATE_CHECK_IN = "LATE_CHECK_IN"
-    SOON_CHECK_OUT = "SOON_CHECK_OUT"
-    LACK_WORKING_HOURS = "LACK_WORKING_HOURS"
-    INVALID = "INVALID"
