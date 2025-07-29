10ccf9b1963a3c130581ede280e6939c32b26837
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 <tmduc1129@gmail.com>
2025-07-29 11:13:12 +0700

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
 # app run here
-import logging
-from fastapi import FastAPI
-from sqlalchemy.exc import SQLAlchemyError
 
-from fastapi.exceptions import HTTPException
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from apis.routes import router
 
@@ -12,6 +10,13 @@ from exceptions.exceptions import *
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(SQLAlchemyError, sqlalchemy_exception_handler)
